28078b6e51ec531d046d26a063458ff6cd9cee93
Nathan Minissale <Bitterpilot@users.noreply.github.com>
2017-10-27 11:41:01 +0800

    Video 1

--- app/src/main/java/com/stackskills_learning/designingtheinterface/MainActivity.java
+++ app/src/main/java/com/stackskills_learning/designingtheinterface/MainActivity.java
@@ -2,12 +2,23 @@ package com.stackskills_learning.designingtheinterface;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = (Button) findViewById(R.id.mButton);
+        mTextView = (TextView) findViewById(R.id.mTextView);
+
+        //Change the tile of the button
+        mButton.setText(R.string.button_name);
     }
 }
