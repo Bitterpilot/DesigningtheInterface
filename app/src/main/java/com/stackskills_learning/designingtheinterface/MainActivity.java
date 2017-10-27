8fa1f76375c7e8c2ef0acf9b61c5d78a6ccdcf59
Nathan Minissale <Bitterpilot@users.noreply.github.com>
2017-10-27 15:02:29 +0800

    Video 3 - take input and display on the screen

--- app/src/main/java/com/stackskills_learning/designingtheinterface/MainActivity.java
+++ app/src/main/java/com/stackskills_learning/designingtheinterface/MainActivity.java
@@ -4,12 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mButton;
     private TextView mTextView;
+    private EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
         mButton = (Button) findViewById(R.id.mButton);
         mTextView = (TextView) findViewById(R.id.mTextView);
+        mEditText = (EditText) findViewById(R.id.editText);
 
         //Change the tile of the button
         mButton.setText(R.string.button_name);
@@ -25,8 +28,11 @@ public class MainActivity extends AppCompatActivity {
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String enteredText;
+                enteredText = mEditText.getText().toString();
+
                 mTextView.setVisibility(View.VISIBLE);
-                mTextView.setText(R.string.show_text);
+                mTextView.setText(enteredText);
             }
         });
     }
