679aa27c600225794735461986ee62a653c5fc2c
Nathan Minissale <Bitterpilot@users.noreply.github.com>
2017-10-27 17:20:13 +0800

    Video 1 - A static colour has been set

--- app/src/main/java/com/stackskills_learning/designingtheinterface/MainActivity.java
+++ app/src/main/java/com/stackskills_learning/designingtheinterface/MainActivity.java
@@ -1,13 +1,23 @@
 package com.stackskills_learning.designingtheinterface;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View windowView;
+    private Button tryMeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        windowView = findViewById(R.id.windowViewID);
+        windowView.setBackgroundColor(Color.RED);
+
     }
 }
