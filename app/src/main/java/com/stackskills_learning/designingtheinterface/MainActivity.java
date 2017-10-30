0551a87ebb24313c5ee2c32d52ce31e858bc70dd
Nathan Minissale <Bitterpilot@users.noreply.github.com>
2017-10-30 16:18:22 +0800

    Video 2 - all done

--- app/src/main/java/com/stackskills_learning/designingtheinterface/MainActivity.java
+++ app/src/main/java/com/stackskills_learning/designingtheinterface/MainActivity.java
@@ -7,24 +7,36 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     private View windowView;
     private Button tryMeButton;
+    //array for colours
+    private int[] colors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        colors = new int[]{Color.BLACK, Color.BLUE, Color.CYAN, Color.DKGRAY, Color.GRAY, Color.GREEN, Color.LTGRAY, Color.MAGENTA, Color.RED, Color.WHITE, Color.YELLOW};
+
         windowView = findViewById(R.id.windowViewID);
-        windowView.setBackgroundColor(Color.RED);
+        //windowView.setBackgroundColor(colors[1]);
 
         tryMeButton = (Button) findViewById(R.id.tryMeButton);
         tryMeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("Test","Tap");
+                int colorArrayLength = colors.length;
+                Random random = new Random();
+                int randomNum = random.nextInt(colorArrayLength);
+
+                windowView.setBackgroundColor(colors[randomNum]);
+
+//                Log.d("Random", String.valueOf(randomNum));
             }
         });
 
